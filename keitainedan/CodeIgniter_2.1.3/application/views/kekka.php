bbd12173b97c39c7f9ce2ce2bd445bff2eb47ba4
tefunamu <chounantakahiro@chounantakahiro-no-MacBook-Air.local>
2014-02-10 23:42:17 +0900

    z

--- keitainedan/CodeIgniter_2.1.3/application/views/kekka.php
+++ keitainedan/CodeIgniter_2.1.3/application/views/kekka.php
@@ -1,1 +1,16 @@
-結果docomo<?php echo $docomo_ryokin; ?>円</br>au<?php echo $au_ryokin; ?>円</br>softbank<?php echo $softbank_ryokin; ?>円</br>入力内容<?php echo $cyaria; ?><?php echo $tushinryo; ?><?php echo $kaisen_tv; ?><?php echo $kisyu; ?><?php echo $ruta; ?><?php echo $tuuwaryoukin; ?><?php echo $gakusei; ?><a href="<?php echo base_url("index.php"); ?>">トップ</a><a href="book">book</a>
+結果
+docomo<?php echo $docomo_ryokin; ?>円</br>
+au<?php echo $au_ryokin; ?>円</br>
+softbank<?php echo $softbank_ryokin; ?>円</br>
+
+入力内容
+<?php echo $cyaria; ?>
+<?php echo $tushinryo; ?>
+<?php echo $kaisen_tv; ?>
+<?php echo $kisyu; ?>
+<?php echo $ruta; ?>
+<?php echo $tuuwaryoukin; ?>
+<?php echo $gakusei; ?>
+
+<a href="<?php echo base_url("index.php"); ?>">トップ</a>
+<a href="book">book</a>
