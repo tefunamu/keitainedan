65fbd12721c0590ea662e4486ffc8c019f973994
tefunamu <chounantakahiro@chounantakahiro-no-MacBook-Air.local>
2014-02-20 22:20:59 +0900

    キャリアの画面表示

--- keitainedan/CodeIgniter_2.1.3/application/views/top.php
+++ keitainedan/CodeIgniter_2.1.3/application/views/top.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" href="css/welcome.css">
 <link href="js/pagefile.js" type="text/javascript" />
 <img src="images/book.png" alt=""></br>
-<a href="index.php/welcome/cyaria">診断スタート！</a>
+<a href="index.php/welcome/kyaria">診断スタート！</a>
