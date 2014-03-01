20118d5e769a2381fb86d78fd52562bcf1f965c4
tefunamu <chounantakahiro@chounantakahiro-no-MacBook-Air.local>
2014-03-01 23:30:10 +0900

    がぞう

--- keitainedan/CodeIgniter_2.1.3/application/views/top.php
+++ keitainedan/CodeIgniter_2.1.3/application/views/top.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="css/welcome.css">
 <link href="js/pagefile.js" type="text/javascript" />
-<img src="images/book.png" alt=""></br>
+<img src="images/renew.jpg" alt=""></br>
+<background="images/background.jpg" ></br>
 <a href="index.php/welcome/kyaria">診断スタート！</a>
