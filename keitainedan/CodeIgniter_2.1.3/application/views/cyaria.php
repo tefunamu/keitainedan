6d0b9c66541383b50c390ed4485dce8364c1af2f
tefunamu <chounantakahiro@chounantakahiro-no-MacBook-Air.local>
2014-05-12 22:22:38 +0900

    a

--- keitainedan/CodeIgniter_2.1.3/application/views/cyaria.php
+++ keitainedan/CodeIgniter_2.1.3/application/views/cyaria.php
@@ -3,6 +3,7 @@
 <img src="../../images/book.png" alt=""></br>
 <form action="kisyu">
 <br><br><br><br><br><br><br><br>
+<a img src="images/hon.gif" alt="judge"></a>
 <input type="radio" name="kyaria" value="docomo">docomo
 <input type="radio" name="kyaria" value="au">au
 <input type="radio" name="kyaria" value="softbanak">softbank
