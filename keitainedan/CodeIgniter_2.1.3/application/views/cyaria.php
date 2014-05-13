ed7c27c6e9a5fa5c712da00f7fe1e7c7cba9ffef
siny <ycossiny@gmail.com>
2014-05-12 23:06:53 -0700

    多分全部なおった
    全パターンテストしたいけどめんどくｓ

--- keitainedan/CodeIgniter_2.1.3/application/views/cyaria.php
+++ keitainedan/CodeIgniter_2.1.3/application/views/cyaria.php
@@ -7,8 +7,13 @@
 <input type="radio" name="kyaria" value="docomo">docomo
 <input type="radio" name="kyaria" value="au">au
 <input type="radio" name="kyaria" value="softbanak">softbank
+<br>
+学生ですか
+<br>
+<input type="radio" name="gakusei" value="gakusei">学生である
+<input type="radio" name="gakusei" value="kazoku">家族に学生がいる
+<input type="radio" name="gakusei" value="kara">いない
 
-<input type="checkbox" name="gakusei" value="gakusei">学生である
 
 <input type="submit"></a>
 
--- keitainedan/CodeIgniter_2.1.3/application/views/kekka.php
+++ keitainedan/CodeIgniter_2.1.3/application/views/kekka.php
@@ -9,10 +9,9 @@ softbank<?php echo $_SESSION['softbank_ryokin']; ?>円</br>
 入力内容<Br>
 入力したキャリアは<?php echo $_SESSION['kyaria']?><Br>
 入力した通話時間は<?php echo $_SESSION['tuuwazikan'] ?><Br>
-入力した回線は<?php echo $_SESSION['kaisen']; ?><Br>
+入力した提携回線、またはルーターは<?php echo $_SESSION['kaisen']; ?><Br>
 入力した機種は<?php echo $_SESSION['kisyu']; ?><Br>
-入力したルーターは<?php echo $_SESSION['ruta']; ?><Br>
-入力した提携回線は<?php echo $_SESSION['packet']; ?><Br>
+入力したパケット数は<?php echo $_SESSION['packet']; ?><Br>
 入力した学生は<?php echo $_SESSION['gakusei']; ?><Br>
 
 <a href="<?php echo base_url("index.php"); ?>">トップ</a>
